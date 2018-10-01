14b1cc0e24e553d96a2e7d5437eacd25013db50d
Wei Fei <weifei6441@gmail.com>
2018-10-01 01:44:53 -0700

    20181001->011

---
+++ A011_ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+public class A011_ContainerWithMostWater {
+    public static void main(String[] args) {
+        A011_ContainerWithMostWater solution = new A011_ContainerWithMostWater();
+        int[] inputs = {1,8,6,2,5,4,8,3,7};
+        int output = solution.maxArea(inputs);
+        System.out.println(output);
+    }
+
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        int n = height.length;
+        //left pointer points to the left most bar, right pointer points to the right most bar
+        //Always move the pointer with shorter length
+        for (int i = 0, j = n - 1; i < j;) {
+            result = Math.max(result, Math.min(height[i], height[j]) * (j - i));
+            //if the left bar length is smaller, move the left pointer by 1
+            if (height[i] < height[j]) {
+                i++;
+            }
+            //if the right bar length is smaller or equal, move the right pointer by 1
+            else {
+                j--;
+            }
+        }
+        return result;
+    }
+}
