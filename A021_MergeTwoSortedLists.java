d6ae6523f6a4a0f7b557b96206ee0371834e9c48
Wei Fei <weifei6441@gmail.com>
2018-11-02 21:19:45 -0700

    20181102Review->021

--- A021_MergeTwoSortedLists.java
+++ A021_MergeTwoSortedLists.java
@@ -26,17 +26,22 @@ public class A021_MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
+            //THIS IS RECURSION EXIT, and will pop items from recursive stack
             return l2;
         }
         if (l2 == null) {
             return l1;
         }
+
+        //find min val, and recursively append the rest
+        //DO NOT FORGET to return l1 or l2
         if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
+            l1.next =  mergeTwoLists(l1.next, l2);
+            //THIS IS FUNCTION EXIT
             return l1;
         }
         else {
-            l2.next = mergeTwoLists(l2.next, l1);
+            l2.next =  mergeTwoLists(l1, l2.next);
             return l2;
         }
     }
