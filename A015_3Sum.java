ceaeacd2cc33a0268601778ec7ebd945645f0e85
Wei Fei <weifei6441@gmail.com>
2018-11-04 23:23:03 -0800

    20181104Review->015

--- A015_3Sum.java
+++ A015_3Sum.java
@@ -21,25 +21,31 @@ public class A015_3Sum {
         if (nums == null || nums.length == 0) {
             return results;
         }
+        //sort the array because it is easier to deduplicate
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
             if (i == 0 || (i > 0 && nums[i - 1] != nums[i])) {
                 int twoSum = 0 - nums[i];
                 for (int low = i + 1, high = nums.length - 1; low < high;) {
                     if (nums[low] + nums[high] == twoSum) {
+                        //we found correct sum, add to results
                         results.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        //deduplicate
                         while (low < high && nums[low] == nums[low + 1]) {
                             low++;
                         }
                         while (low < high && nums[high] == nums[high - 1]) {
                             high--;
                         }
+                        //used both of two numbers, move both of two indexes
                         low++;
                         high--;
                     }
+                    // if sum is greater than target, we need a smaller number
                     else if (nums[low] + nums[high] > twoSum) {
                         high--;
                     }
+                    //if sum is less than target, we need a grater number
                     else {
                         low++;
                     }
