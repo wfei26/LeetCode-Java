4279df9fa41bcc03bdf39c7b9c37c270401a4636
Wei Fei <weifei6441@gmail.com>
2018-11-05 22:19:07 -0800

    20181105Review->007

--- A007_ReverseInteger.java
+++ A007_ReverseInteger.java
@@ -8,9 +8,13 @@ public class A007_ReverseInteger {
     public int reverse(int x) {
         int result = 0;
 
+        //iteratively get last digit of the rest of number 
         for (; x != 0; x /= 10) {
             int lastDigit = x % 10;
+            //new number is equal to previous * 10 + current last digit
             int newNumber = result * 10 + lastDigit;
+
+            //check if new integer over than bound
             if ((newNumber - lastDigit) / 10 != result) {
                 return 0;
             }
