aabe3e98cf0e8b10e63126a3aeeb404797c1a149
Wei Fei <weifei6441@gmail.com>
2019-03-16 22:50:07 -0700

    20190316 Review->419

--- A419_BattleshipsInABoard.java
+++ A419_BattleshipsInABoard.java
@@ -20,13 +20,15 @@ public class A419_BattleshipsInABoard {
                 if (board[i][j] == '.') {
                     continue;
                 }
-                if (i > 0 && board[i-1][j] == 'X') {
+                else if (i > 0 && board[i-1][j] == 'X') {
                     continue;
                 }
-                if (j > 0 && board[i][j-1] == 'X') {
+                else if (j > 0 && board[i][j-1] == 'X') {
                     continue;
                 }
-                count++;
+                else {
+                    count++;
+                }
             }
         }
 
