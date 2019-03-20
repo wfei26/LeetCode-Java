ea199194e517eecd6eb603b8efefd9f88e3a3559
Wei Fei <weifei6441@gmail.com>
2019-03-19 21:23:04 -0700

    20190319 Review->076,153

--- A076_MinimumWindowSubstring.java
+++ A076_MinimumWindowSubstring.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class A076_MinimumWindowSubstring {
     public static void main(String[] args) {
@@ -9,45 +10,52 @@ public class A076_MinimumWindowSubstring {
         System.out.println(myResult);
     }
 
+    /**
+     * classical sliding window problem
+     * Step 1: count frequency of each character in t, also count how many different characters exist in t
+     * Step 2: keep a sliding window, move right pointer until the window contains all characters from t
+     * Step 3: iteratively move left pointer and update min result until the window does not contain all characters from t
+     * */
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() < t.length()) {
+        if (s.length() == 0 || t.length() == 0) {
             return "";
         }
 
-        String result = "";
         int count = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < t.length(); i++) {
-            if (map.containsKey(t.charAt(i))) {
-                map.put(t.charAt(i), map.get(t.charAt(i)) + 1);
-            }
-            else {
-                map.put(t.charAt(i), 1);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
                 count++;
             }
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
+        String result = "";
         int minLen = s.length() + 1;
         for (int left = 0, right = 0; right < s.length(); right++) {
-            if (map.containsKey(s.charAt(right))) {
-                map.put(s.charAt(right), map.get(s.charAt(right)) - 1);
-                if (map.get(s.charAt(right)) == 0) {
+            char curChar = s.charAt(right);
+            if (map.containsKey(curChar)) {
+                map.put(curChar, map.get(curChar) - 1);
+                if (map.get(curChar) == 0) {
                     count--;
                 }
+            }
 
-                while (count == 0) {
-                    if (right - left + 1 < minLen) {
-                        minLen = right - left + 1;
-                        result = s.substring(left, left + minLen);
-                    }
-                    if (map.containsKey(s.charAt(left))) {
-                        map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
-                        if (map.get(s.charAt(left)) > 0) {
-                            count++;
-                        }
-                    }
-                    left++;
+            // iteratively move left pointer and update min result until the window does not contain all characters from t
+            while (count == 0) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    result = s.substring(left, right + 1);
                 }
+                if (map.containsKey(s.charAt(left))) {
+                    map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
+                    // WARNING: MUST determine whether frequency of current character is greater than 0 or not
+                    // since there may exist some characters with negative frequencies: eg. s = abababc, t = abc
+                    if (map.get(s.charAt(left)) > 0) {
+                        count++;
+                    }
+                }
+                left++;
             }
         }
         return result;
--- A153_FindMinimumInRotatedSortedArray.java
+++ A153_FindMinimumInRotatedSortedArray.java
@@ -7,17 +7,21 @@ public class A153_FindMinimumInRotatedSortedArray {
     }
 
     public int findMin(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return -1;
+        if (nums.length == 0) {
+            return 0;
         }
+        int left = 0;
+        int right = nums.length - 1;
 
-        int left = 0, right = nums.length - 1;
         while (left < right) {
-            //Key point: if left < right, left must be the minimum
+            // KEY POINT: once we find left number is smaller than right number, we find the correct answer
             if (nums[left] < nums[right]) {
                 return nums[left];
             }
-            int mid = (right + left) / 2;
+
+            int mid = left + (right - left) / 2;
+            // WARNING: MUST USE <= at here. eg. for the input 3,1,2, we will search to the left most element, the last
+            // condition will determine whether nums[0] <= nums[0]. If we use <, we cannot get the index 1 as our answer
             if (nums[left] <= nums[mid]) {
                 left = mid + 1;
             }
